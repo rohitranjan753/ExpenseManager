9b84daf2b0c132745fb8f3998b53365a91237042
Rohit Ranjan <rohitgns54321@gmail.com>
2023-03-14 10:29:46 +0530

    add new things

--- lib/main.dart
+++ lib/main.dart
@@ -6,31 +6,53 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   final List<Transaction> trasnsaction = [
-    Transaction(id: 't1',title: 'New Shoes',amount: 69.55, date: DateTime.now()),
-    Transaction(id: 't2',title: 'New Pant',amount: 50.88, date: DateTime.now()),
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.55, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'New Pant', amount: 50.88, date: DateTime.now()),
   ];
- 
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter App"),
-      ),
-      body: Column(
-        children: [
-          Card(
-            elevation: 10,
-            child: Container(child: Text('chart'),
-            width: 100,),
-          ),
-          Card(
-            child: Text('List of tx'),
-          ),
-        ],
+    return Material(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter App"),
+        ),
+        body: Column(
+          children: [
+            Card(
+              elevation: 10,
+              child: Container(
+                child: Text('chart'),
+                width: 100,
+              ),
+            ),
+            Column(
+              children: trasnsaction.map((tx) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        child: Text(
+                          tx.amount.toString(),
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Text(tx.title),
+                          Text(tx.date.toString()),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/transaction.dart
+++ lib/transaction.dart
@@ -2,15 +2,15 @@ import 'package:flutter/material.dart';
 
 
 class Transaction{
-  final String? id;
-  final String? title;
-  final double? amount;
-  final DateTime? date;
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
 
   Transaction({
-    @required this.id,
-    @required this.title,
-    @required this.amount,
-    @required this.date
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.date
   });
 }
